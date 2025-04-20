ed827677239badcbea35381308062204bb817978
Ruchi Thakur <163337765+Ruchi-Thakur-105@users.noreply.github.com>
2025-04-20 23:10:31 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
 
 router.post("/", async (req, res) => {
 	try {
